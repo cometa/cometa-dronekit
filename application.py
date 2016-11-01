fdf97eb85eca2e42dd63ef71ad0a92087a147e42
Marco Graziano <marco@visiblenergy.com>
2016-11-01 15:15:52 -0700

    initial checkin

---
+++ application.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+"""
+Author: Marco Graziano
+"""
+__license__ = """
+Copyright 2016 Visible Energy Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cometalib import CometaClient
+import time
+import sys, getopt
+import string
+import json
+import subprocess
+
+from uuid import getnode as get_mac
+from time import gmtime, strftime
+
+from runtime import Runtime
+
+def message_handler(msg, msg_len):
+	"""
+		The message handler is the Cometa receive callback. 
+		Every time the Cometa library receives a message for this device the message_handler is invoked.
+	"""
+	try:
+		c = json.loads(msg)
+	except Exception, e:
+		print "Error in parsing the message: ", msg
+		return "{\"msg\":\"Invalid JSON object.\"}"
+
+	if 'cmd' in c:
+		print "Command received: ", c['cmd']	#DEBUG
+		command = c['cmd']
+		# execute the command in a shell on the device
+		out = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+		return out
+	else:
+		print "Invalid command."
+		return "{\"msg\":\"Invalid command.\"}"
+	
+def main(argv):
+
+	Runtime.init_runtime()
+	syslog = Runtime.syslog
+	config = Runtime.read_config()
+
+	cometa_server = config['cometa_server']
+	cometa_port = config['cometa_port']
+	application_id = config['cometa_app']
+	# if not specified use the machine's MAC address as Cometa device ID
+	device_id = Runtime.get_mac_address()
+
+	# ------------------------------------------------ #
+	print "Cometa client started.\r\nParams: cometa_server:", cometa_server, "cometa_port:", cometa_port, "application_id:", application_id, "device_id:", device_id
+
+	# Instantiate a Cometa object
+	com = CometaClient(cometa_server, cometa_port, application_id)
+	# Set debug flag
+	# com.debug = True
+
+	# Bind the message_handler() callback. The callback is doing the function of respoding
+	# to remote requests and handling the core part of the work of the application.
+	com.bind_cb(message_handler)
+
+	# Attach the device to Cometa.
+	ret = com.attach(device_id, "DroneKit")
+	if com.error != 0:
+		print "(FATAL) Error in attaching to Cometa.", com.perror()
+		sys.exit(2)
+
+	# When attach is successful the server returns an object of the format:
+	# {"msg":"200 OK","heartbeat":60,"timestamp":1441405206}
+	try:
+		ret_obj = json.loads(ret)
+	except Exception, e:
+		print "(FATAL) Error in parsing the message returned after attaching to Cometa. Message:", ret
+		sys.exit(2)
+
+	print "Device [%s] attached to Cometa. Server timestamp: %d" % (device_id, ret_obj['timestamp'])
+	if com.debug:
+		print "Server returned:", ret
+
+	# Application main loop.
+	while True:
+		"""
+			Send a data event with the device ID and the current time upstream to the Cometa
+			server every minute. This is to demonstrate use of the asynchronous data event messages.
+			Once a data event message is received by Cometa, the message is propagated to all opened 
+			device Websockets. If a Webhook for the application is specified in the Cometa configuration
+			file /etc/cometa.conf the message is also posted to the configured Webhook.
+		"""
+		time.sleep(60)
+		now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		msg = "{\"id\":\"%s\",\"time\":\"%s\"}" % (device_id, now)
+
+		if com.send_data(msg) < 0:
+			print "Error in sending data."
+		else:
+			if com.debug:
+				print "sending data event.", msg
+
+	print "***** should never get here"
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ runtime.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+"""
+Low-level Board run-time support for Cometa IoT devices.
+
+Author: Marco Graziano (marco@visiblenergy.com)
+"""
+__license__ = """
+Copyright 2016 Visible Energy Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__all__ = ["Runtime"]
+
+import time
+import threading
+import json
+import subprocess
+import os
+from uuid import getnode as get_mac
+
+# Default values
+DCT_FILENAME = 'config.json'
+
+# This module uses the following enviromental variables:
+#	COMETA_APIKEY
+#	COMETA_SERVER
+#	COMETA_PORT
+
+#@runtimeclass
+class Runtime(object):
+    """
+    Run-time support for Cometa IoT Devices.
+    Note: the class decorator method runs after the class is created,
+    """
+
+    # Internal system time - resolution 1Hz
+    __systime = 0
+
+    # System status
+    __status = "unknown"
+
+    # Thread IDs
+    __thtime = None     # Update __systime thread
+
+    def __init__(self):
+        """
+        The Runtime object instance constructor.
+        """
+        pass
+
+    # Complete class initialization after loading.
+    @classmethod
+    def init_runtime(klass):
+        # start the update systime thread
+        if klass.__thtime == None:
+            klass.__thtime = threading.Thread(target=klass._update_systime)
+            klass.__thtime.daemon = True  # force to exit on SIGINT
+            klass.__thtime.start()
+        klass.__status = "OK"
+    # 
+    #-----------------------------------------------
+    #
+    # Update systime every second (thread)
+    @classmethod
+    def _update_systime(klass):
+        """
+        Thread to update the system time every second. (private)
+        """
+        while True:
+            time.sleep(1)
+            klass.__systime += 1    # no need for lock
+
+    # Force systime to a new value
+    @classmethod
+    def set_systime(klass, time):
+        """
+        Set the system time to a new value.
+        """
+        klass.__systime = time
+
+    # Get current systime
+    @classmethod
+    def get_systime(klass):
+        return klass.__systime
+
+    # Get host systime
+    @classmethod
+    def get_hostsystime(klass):
+    	return int(time.time())
+
+    # Set current status
+    @classmethod
+    def set_status(klass, new_status):
+        klass.__status = new_status
+
+    # Get current status
+    @classmethod
+    def get_status(klass):
+        return klass.__status
+    # 
+    #-----------------------------------------------
+    #
+
+    # 
+    #-----------------------------------------------
+    #
+    # Read config from DCT (Device Configuration Table simulated in a file)
+    @classmethod
+    def read_config(klass):
+        # read the configuration from the DCT
+        try:
+            f = open(DCT_FILENAME)
+            content = json.loads(f.read())['config']
+
+            # get Cometa API Key and Server name from the environment
+            if not 'cometa_app' in content:
+            	content['cometa_app'] =  os.environ['COMETA_APIKEY']
+            if not 'cometa_server' in content:
+            	content['cometa_server'] = os.environ['COMETA_SERVER']
+            if not 'cometa_port' in content: 
+            	content['cometa_port'] = int(os.environ['COMETA_PORT'])
+            return content
+        except Exception as e:
+            print e
+            return ""
+
+    # Read all DCT
+    @classmethod
+    def read_dct(klass):  
+        # read the DCT
+        try:
+            f = open(DCT_FILENAME)
+            #content = f.read().replace(u'\n', u'').replace(u'\r', u'')
+            content = json.loads(f.read().replace('\n', '').replace('\r', ''))
+            # get Cometa API Key and Server name from the environment
+            if not 'cometa_app' in content['config']:
+            	content['config']['cometa_app'] =  os.environ['COMETA_APIKEY'] 
+            if not 'cometa_server' in content['config']:
+            	content['config']['cometa_server'] = os.environ['COMETA_SERVER']
+            if not 'cometa_port' in content['config']:         
+            	content['config']['cometa_port'] = int(os.environ['COMETA_PORT'])          
+            return content
+        except Exception as e:
+            return ""
+
+    # Get device serial number
+    # in Linux hosts is the least six digits of the MAC address
+    @classmethod
+    def get_serial(klass):
+        """
+        Return an hex string with the current network interface MAC address.
+        """
+        # TODO: testing only
+        #return 'A7A7A8'
+
+        mac = ''
+        m = get_mac()
+        #for i in range(0, 12, 2):
+        for i in range(6, 12, 2):
+            mac += ("%012X" % m)[i:i+2]
+        return mac
+
+    # Get MAC address of current network interface
+    @classmethod
+    def get_mac_address(klass):
+        """
+        Return an hex string with the current network interface MAC address.
+        """
+        # TODO: testing only
+        #return 'A7A7A8'
+
+        return get_mac()
+
+    # Get info of the current network interface
+    @classmethod
+    def get_network_info(klass):
+        # TODO: testing only
+        #return {'gateway': 'N/A', 'ip_address': 'N/A'}
+
+        # get network info
+        s = subprocess.Popen('ip route', shell=True, stdout=subprocess.PIPE).stdout.read()
+        reply = {}
+        reply['gateway'] = s.split('default via')[-1].split()[0]
+        reply['ip_address'] = s.split('src')[-1].split()[0]
+        return reply
+
+    # Simple system logger
+    @classmethod
+    def syslog(klass, msg, escape=False):
+        """
+        Simple system logger.
+        """
+        if escape:
+            #print ("[%d] %s" % (klass.__systime, msg.replace(u'\n', u'#015').replace(u'\r', u'#012')))
+            print ("[%d] %s" % (klass.__systime, msg.replace('\n', '#015').replace('\r', '#012')))
+        else:
+            print ("[%d] %s" % (klass.__systime, msg))
+
