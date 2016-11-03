1953bd737fb198c17c894a64bd8e4452df3a114c
Marco Graziano <marco@visiblenergy.com>
2016-11-03 15:51:14 -0700

    added speed and other remote commands

--- application.py
+++ application.py
@@ -26,7 +26,8 @@ from time import gmtime, strftime
 from runtime import Runtime
 from cometalib import CometaClient
 
-from dronekit import connect, VehicleMode, LocationGlobal
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+import utils
 
 # JSON-RPC errors
 JSON_RPC_PARSE_ERROR = '{"jsonrpc": "2.0","error":{"code":-32700,"message":"Parse error"},"id": null}'
@@ -165,6 +166,288 @@ def _set_telemetry_period(params):
 	config['app_params']['event_loop'] = params['period']
 	return {"success": True}
 
+"""
+Set the telemetry params to a new tuple. Pass an array of attribute names to be compatible with JSON/RPC.
+"""
+def _set_telemetry_attributes(params):
+    if type(params) is not list:
+        return {"success": False}
+    tparams = tuple(params)
+    allowed = ('attitude','location','velocity','battery','state','groundspeed','airspeed','mode','armed')
+    for x in tparams:
+        if x not in allowed:
+            return {"success": False}
+    telemetry_attributes_names = tparams
+    return {"success": True}
+
+"""
+Vehicle takeoff to the specified altitude.
+{"alt": 4.3}
+"""
+def _takeoff(params):
+	if type(params) is not dict:
+		return {"success": False}
+	if ('lat') not in params.keys():
+		return {"success": False}
+	vehicle.simple_takeoff(params['alt'])
+
+"""
+Moves the vehicle to the absolute or relative position specified.
+{"lat": -34.2, "lon": 149.2, "alt" = 3.0, "relative": true}
+"""
+def _goto(params):
+	if type(params) is not dict:
+		return {"success": False}
+	if ('lat' and 'lon' and 'alt' and 'relative') not in params.keys():
+		return {"success": False}
+	if relative:
+		dest = LocationGlobalRelative(lat, lon, alt)
+	else:
+		dest = LocationGlobal(params['lat'], params['lon'], params['alt'])
+	vehicle.simple_goto(dest)
+	return {"success": True}
+
+"""
+Moves the vehicle to the destination North and East meters of the current position.
+{"dNorth": 1.5, "dEast": 10.2}
+"""
+def _goto_destination(params):
+	if type(params) is not dict:
+		return {"success": False}
+	if ('dNorth' and 'dEast') not in params.keys():
+		return {"success": False}
+	curLocation = vehicle.location.global_relative_frame
+	dest = utils.get_location_meters(curLocation, dNorth, dEast)
+	vehicle.simple_goto(dest)
+	return {"success": True}
+
+"""
+Moves the vehicle to the global position specified.
+{"lat": -34.2, "lon": 149.2, "alt" = 3.0}
+"""
+def _goto_position_target_global_int(params):
+    """
+    Send SET_POSITION_TARGET_GLOBAL_INT command to request the vehicle fly to a specified LocationGlobal.
+
+    For more information see: https://pixhawk.ethz.ch/mavlink/#SET_POSITION_TARGET_GLOBAL_INT
+
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    if type(params) is not dict:
+		return {"success": False}
+    if ('lat' and 'lon' and 'alt') not in params.keys():
+		return {"success": False}    
+    lat = params['lat']
+    lon = params['lon']
+    alt = params['alt']
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        0b0000111111111000, # type_mask (only speeds enabled)
+        lat * 1e7, # lat_int - X Position in WGS84 frame in 1e7 * meters
+        lon * 1e7, # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        alt, # alt - Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
+        0, # X velocity in NED frame in m/s
+        0, # Y velocity in NED frame in m/s
+        0, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
+"""
+Moves the vehicle to the global position specified.
+{"north": -34.2, "east": 149.2, "down" = 3.0}
+"""
+def _goto_position_target_local_ned(params):
+    """	
+    Send SET_POSITION_TARGET_LOCAL_NED command to request the vehicle fly to a specified 
+    location in the North, East, Down frame.
+
+    It is important to remember that in this frame, positive altitudes are entered as negative 
+    "Down" values. So if down is "10", this will be 10 metres below the home altitude.
+
+    Starting from AC3.3 the method respects the frame setting. Prior to that the frame was
+    ignored. For more information see: 
+    http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_local_ned
+
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+
+    """
+    if type(params) is not dict:
+		return {"success": False}
+    if ('north' and 'east' and 'down') not in params.keys():
+		return {"success": False}  
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111111000, # type_mask (only positions enabled)
+        north, east, down, # x, y, z positions (or North, East, Down in the MAV_FRAME_BODY_NED frame
+        0, 0, 0, # x, y, z velocity in m/s  (not used)
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
+"""
+ Point vehicle at a specified heading (in degrees) relative to the direction or absolut according to the specified value.
+ {"heading": 45.0, relative": false}
+"""
+def _condition_yaw(params):
+    """
+    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+    This method sets an absolute heading by default, but you can set the `relative` parameter
+    to `True` to set yaw relative to the current yaw heading.
+
+    By default the yaw of the vehicle will follow the direction of travel. After setting 
+    the yaw using this function there is no way to return to the default yaw "follow direction 
+    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+    For more information see: 
+    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+    """
+    if type(params) is not dict:
+		return {"success": False}
+    if ('heading' and 'relative') not in params.keys():
+		return {"success": False} 
+    if params['relative']:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        params['heading'],    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
+"""
+Point the camera at the global position specified.
+{"lat": -34.2, "lon": 149.2, "alt" = 3.0}
+"""
+def _point_camera(params):
+    """
+    Send MAV_CMD_DO_SET_ROI message to point camera gimbal at a 
+    specified region of interest (LocationGlobal).
+    The vehicle may also turn to face the ROI.
+
+    For more information see: 
+    http://copter.ardupilot.com/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_do_set_roi
+    """
+    if type(params) is not dict:
+		return {"success": False}
+    if ('lat' and 'lon' and 'alt') not in params.keys():
+		return {"success": False}    
+    lat = params['lat']
+    lon = params['lon']
+    alt = params['alt']
+    # create the MAV_CMD_DO_SET_ROI command
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
+        0, #confirmation
+        0, 0, 0, 0, #params 1-4
+        lat,
+        lon,
+        alt
+        )
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
+"""
+Move vehicle in direction based on specified velocity vectors in the local frame.
+{"velocity_x": 2.3, "velocity_y": 5.0, "velocity_z":0.2}
+"""
+def _send_ned_velocity(params):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+
+    This uses the SET_POSITION_TARGET_LOCAL_NED command with a type mask enabling only 
+    velocity components 
+    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_local_ned).
+    
+    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
+    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
+    velocity persists until it is canceled. The code below should work on either version 
+    (sending the message multiple times does not cause problems).
+    
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    if type(params) is not dict:
+        return {"success": False}
+    if ('velocity_x' and 'velocity_y' and 'velocity_z') not in params.keys():
+        return {"success": False}     
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        params['velocity_x'], params['velocity_y'], params['velocity_z'], # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
+"""
+Move vehicle in direction based on specified velocity vectors in the global frame.
+{"velocity_x": 2.3, "velocity_y": 5.0, "velocity_z":0.2}
+"""
+def _send_global_velocity(params):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+
+    This uses the SET_POSITION_TARGET_GLOBAL_INT command with type mask enabling only 
+    velocity components 
+    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_global_int).
+    
+    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
+    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
+    velocity persists until it is canceled. The code below should work on either version 
+    (sending the message multiple times does not cause problems).
+    
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    if type(params) is not dict:
+        return {"success": False}
+    if ('velocity_x' and 'velocity_y' and 'velocity_z') not in params.keys():
+        return {"success": False}    
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, # lat_int - X Position in WGS84 frame in 1e7 * meters
+        0, # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        0, # alt - Altitude in meters in AMSL altitude(not WGS84 if absolute or relative)
+        # altitude above terrain if GLOBAL_TERRAIN_ALT_INT
+        velocity_x, # X velocity in NED frame in m/s
+        velocity_y, # Y velocity in NED frame in m/s
+        velocity_z, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    return {"success": True}
+
 global rpc_methods
 rpc_methods = ({'name':'shell','function':_shell}, 
 #               {'name':'status','function':show_status}, 
@@ -175,8 +458,18 @@ rpc_methods = ({'name':'shell','function':_shell},
                {'name':'vehicle_parameters','function':_get_vehicle_parameters},
 			   {'name':'set_parameters','function':_set_vehicle_parameters},
 			   {'name':'set_telemetry_period','function':_set_telemetry_period},
+               {'name':'set_telemetry_attributes','function':_set_telemetry_attributes},
                {'name':'home_location','function':_get_home_location},
-               {'name':'set_home_location','function':_set_home_location}
+               {'name':'set_home_location','function':_set_home_location},
+               {'name':'takeoff','function':_takeoff},
+               {'name':'goto','function':_goto},
+               {'name':'goto_destination','function':_goto_destination},
+               {'name':'goto_position_global_int','function':_goto_position_target_global_int},
+               {'name':'goto_position_local_ned','function':_goto_position_target_local_ned},
+               {'name':'condition_yaw','function':_condition_yaw},
+               {'name':'point_camera','function':_point_camera},
+               {'name':'send_ned_velocity','function':_send_ned_velocity},
+               {'name':'send_global_velocity','function':_send_global_velocity},
 )
 
 def message_handler(msg, msg_len):
@@ -195,9 +488,9 @@ def message_handler(msg, msg_len):
 		return JSON_RPC_PARSE_ERROR
 
     # check the message is a proper JSON-RPC message
-    ret,id = check_rpc_msg(req)
+    ret,id = utils.check_rpc_msg(req)
     if not ret:
-        if id and isanumber(id):
+        if id and utils.isanumber(id):
             return JSON_RPC_INVALID_PARAMS_FMT_NUM % id
         if id and isinstance(id, str):
             return JSON_RPC_INVALID_PARAMS_FMT_STR % id
@@ -241,40 +534,6 @@ def get_telemetry():
 	for k in telemetry_attributes_names:
 		ret[k] = attr[k]
 	return ret
-
-# --------------------
-# 
-# Utility functions
-def check_rpc_msg(req):
-    ret = False
-    id = None
-    k = req.keys()
-    # check presence of required id attribute
-    if 'id' in k:
-        id = req['id']
-    else:
-        return ret, id
-    # check object length
-    if (len(k) != 4):
-        return ret, id
-    # check presence of required attributes
-    if (not 'jsonrpc' in k) or (not 'method' in k) or (not 'params' in k):
-        return ret, id
-    # check for version
-    if req['jsonrpc'] != "2.0":
-        return ret, id
-    # valid request
-    return True,id
-
-def isanumber(x):
-    try:
-        int(x)
-    except ValueError:
-        try:
-            float(x)
-        except ValueError:
-            return False
-    return True
 
 # --------------------
 # 
